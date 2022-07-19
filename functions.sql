bf987c5e14c9486d9146c42f82d2bc09b4dc3e24
Diego-Valdiviezo-S <109091399+Diego-Valdiviezo-S@users.noreply.github.com>
2022-07-19 08:56:34 -0600

    Update functions.sql

--- functions.sql
+++ functions.sql
@@ -13,8 +13,38 @@ WHERE FirstName = @NameEmployee
 Return @ProjectID
 END
 
+-- Ejecucion
 DECLARE @RESULTS AS INT
 SELECT @RESULTS = dbo.GetLastProjetIdByEmployeeName('Juan')
 print @RESULTS
 
 -- 6.2
+CREATE FUNCTION HasAnyProject(@FirstName VARCHAR(50))
+RETURNS BIT
+AS
+BEGIN
+DECLARE
+@ProjectBit AS BIT,
+@ProjectNum AS INT
+SELECT @ProjectNum = COUNT(Projects.IdProject)
+FROM Projects
+INNER JOIN Pivote ON Projects.IdProject=Pivote.IdEmployee INNER JOIN Employees
+ON Pivote.IdEmployee=Employees.IdEmployee
+WHERE @FirstName = Employees.FirstName
+GROUP BY Employees.FirstName
+IF (@ProjectNum>1)
+BEGIN
+SET @ProjectBit = 1
+END
+ELSE
+BEGIN
+SET @ProjectBit = 0
+END
+RETURN @ProjectBit
+END
+
+-- Ejecucion
+DECLARE @Result INT
+SELECT @Result = dbo.HasAnyProject('Jose')
+PRINT @Result
+
